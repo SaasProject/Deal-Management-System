0c300c5480c9348acdc4fb84303022f226f41974
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2018-06-04 14:25:10 +0800

    hmm

--- app/deals/dealForm.js
+++ app/deals/dealForm.js
@@ -7,69 +7,40 @@
 
 
     function Controller($scope, $rootScope, $state, $stateParams, $filter, ModulesService, DealsService) {
+
+        //ng-model for a deal
         $scope.dealForm = getInitialDealForm();
-        var tempDealForm = getInitialDealForm();
+
+        //working format for two way conversion
         var DATE_FORMAT = 'MM/dd/yyyy';
 
-        //from jano's code (multiple getModuleByName)
-        $scope.profileFields = [];
-        $scope.processFields = [];
-        $scope.distributionFields = [];
-        $scope.statusFields = [];
-        $scope.contentFields = [];
+        //initialize variables for distribution section
+        $scope.currentFiscalYear = [];
+        $scope.startingMonthYear = new Date();
 
-        function getAllFields() {
-            ModulesService.getModuleByName('dealessential').then(function (response) {
-                $scope.essentialFields = response.fields;
-                $scope.essentialFieldsId = response._id;
-                /* for (var i = 0; i < $scope.essentialFields.length; i++) {
-                    $scope.dealForm.essential[$scope.essentialFields[i].name] = ($scope.essentialFields[i].type !== 'number') ? '' : 0;
-                } */
-            }).catch(function (err) {
-                alert(err.msg_error);
-            });
-            ModulesService.getModuleByName('dealprofile').then(function (response) {
-                $scope.profileFields = response.fields;
-                $scope.profileFieldsId = response._id;
-                /* for (var i = 0; i < $scope.profileFields.length; i++) {
-                    $scope.dealForm.profile[$scope.profileFields[i].name] = ($scope.profileFields[i].type !== 'number') ? '' : 0;
-                } */
-            }).catch(function (err) {
-                alert(err.msg_error);
-            });
-            ModulesService.getModuleByName('dealprocess').then(function (response) {
-                $scope.processFields = response.fields;
-                $scope.processFieldsId = response._id;
-                /* for (var i = 0; i < $scope.processFields.length; i++) {
-                    $scope.dealForm.process[$scope.processFields[i].name] = ($scope.processFields[i].type !== 'number') ? '' : 0;
-                } */
-            }).catch(function (err) {
-                alert(err.msg_error);
-            });
-            ModulesService.getModuleByName('dealdistribution').then(function (response) {
-                $scope.distributionFields = response.fields;
-                $scope.distributionFieldsId = response._id;
-                /* for (var i = 0; i < $scope.distributionFields.length; i++) {
-                    $scope.dealForm.distribution[$scope.distributionFields[i].name] = ($scope.distributionFields[i].type !== 'number') ? '' : 0;
-                } */
-            }).catch(function (err) {
-                alert(err.msg_error);
-            });
-            ModulesService.getModuleByName('dealstatus').then(function (response) {
-                $scope.statusFields = response.fields;
-                $scope.statusFieldsId = response._id;
-                /* for (var i = 0; i < $scope.statusFields.length; i++) {
-                    $scope.dealForm.status[$scope.statusFields[i].name] = ($scope.statusFields[i].type !== 'number') ? '' : 0;
-                } */
-            }).catch(function (err) {
-                alert(err.msg_error);
-            });
-            ModulesService.getModuleByName('dealcontent').then(function (response) {
-                $scope.contentFields = response.fields;
-                $scope.contentFieldsId = response._id;
+        //store in arrays to be able to use ng-repeat
+        $scope.fields = {
+            essential: [],
+            profile: [],
+            process: [],
+            distribution: [],
+            status: [],
+            content: []
+        };
 
+        //get the fields arrays of dealessential, dealprofile, dealprocess, dealstatus, and dealcontent
+        function getAllFields () {
+            ModulesService.getAllModules().then(function (allModules) {
+                var category;
+                var allModulesLength = allModules.length;
+                for (var i = 0; i < allModulesLength; i++) {
+                    //first condition is to make sure that collections that have 'deal' in its name are only processed.
+                    if (allModules[i].name.search('deal') !== -1 && allModules[i].name !== 'deals') {
+                        category = allModules[i].name.replace('deal', '');
+                        $scope.fields[category] = allModules[i].fields;
+                    }
+                }
             }).catch(function (err) {
-                alert(err.msg_error);
             });
         }
 
@@ -79,12 +50,8 @@
         if ($stateParams.ID !== '') {
             //get one then store to $scope.dealForm;
             DealsService.getDealById($stateParams.ID).then(function(aDeal) {
-                $scope.dealForm = aDeal;
-
-                //explicitly convert dates of Due Date, Duration (Start) & Duration (End) to date objects
-                $scope.dealForm.essential['Due Date'] = new Date($scope.dealForm.essential['Due Date']);
-                $scope.dealForm.profile['Duration (Start)'] = new Date($scope.dealForm.profile['Duration (Start)']);
-                $scope.dealForm.profile['Duration (End)'] = new Date($scope.dealForm.profile['Duration (End)']);
+                //use true to convert datestrings to date objects
+                $scope.dealForm = preProcess(aDeal, true);
 
              }).catch(function() {
                 $scope.message = 'Cannot find the deal';
@@ -93,30 +60,11 @@
 
         $scope.submit = function () {
             //use Object.assign(target, source) instead
-            Object.assign(tempDealForm, $scope.dealForm);
-   
-            //explicitly convert dates of Due Date, Duration (Start) & Duration (End) to datestring of prescribed format
-            /* tempDealForm.essential['Due Date'] = $filter('date')(tempDealForm.essential['Due Date'], DATE_FORMAT);
-            tempDealForm.profile['Duration (Start)'] = $filter('date')(tempDealForm.profile['Duration (Start)'], DATE_FORMAT);
-            tempDealForm.profile['Duration (End)'] = $filter('date')(tempDealForm.profile['Duration (End)'], DATE_FORMAT); */
+            var tempDealForm = {};
+            Object.assign(tempDealForm, $scope.dealForm);            
 
-            console.log(tempDealForm);
-
-            //convert all date objects to datestring
-            angular.forEach(tempDealForm, function(fields, category){
-                if (category === 'essential' 
-                || category === 'profile' 
-                || category === 'process' 
-                || category === 'distribution' 
-                || category === 'status' 
-                || category === 'content') {
-                    angular.forEach(tempDealForm[category], function(value, key){
-                        if (tempDealForm[category][key] instanceof Date) {
-                            tempDealForm[category][key] = $filter('date')(tempDealForm[category][key], DATE_FORMAT);
-                        }
-                    });
-                }
-            });
+            //use false to convert date objects to datestrings
+            tempDealForm = preProcess(tempDealForm, false);
 
             if (tempDealForm._id === undefined) {
                 DealsService.addDeal(tempDealForm)
@@ -137,7 +85,6 @@
             }            
         }
 
-
         //do not initialize dates to the current date since it is not required
         function getInitialDealForm() {
             return {
@@ -151,7 +98,6 @@
 
                 },
                 distribution: {
-                    fiscalYear: [new Date().getFullYear(), new Date().getFullYear() + 1],
                     total: {
                         resource: 0,
                         revenue: 0,
@@ -178,18 +124,15 @@
             };
         }
 
-        function tryFunction () {
-
-        }
-
-        $scope.currentFiscalYear = [];
-        $scope.startingMonthYear = new Date();
-
         $scope.getCurrentFiscalYear = function () {
+            //reset the array
             $scope.currentFiscalYear = [];
+            
+            //assign to the current date if input is null
             if ($scope.startingMonthYear === null) {
                 $scope.startingMonthYear = new Date();
             }
+            //+ 1 since javascript months starts with 0
             var setMonth = $scope.startingMonthYear.getMonth() + 1; 
             var setYear = $scope.startingMonthYear.getFullYear();
             var temp = '';
@@ -207,11 +150,41 @@
 
                 $scope.currentFiscalYear.push(temp);
 
+                //use modulo to set i as 1 instead of 13
                 i = (i % 12 === 0) ? 1 : (i + 1);
             } while (i != setMonth);
         }
 
         $scope.getCurrentFiscalYear();
+
+        function preProcess(dealForm, isLoaded) {
+            var tempObject = dealForm;
+            //use $scope.fields and iterate each array
+            angular.forEach($scope.fields, function(fields, category) {
+                var i, currentField, arrayLength = fields.length;
+                for(i = 0; i < arrayLength; i++) {
+                    currentField = $scope.fields[category][i];
+                    //check the input type and process only dates
+                    if(currentField.type === 'date') {
+                        //if it is loaded from database, convert the datestring to date object
+                        //else, convert the date object to a datestring with the prescribed format
+                        tempObject[category][currentField.name] = (isLoaded) ? 
+                        new Date(tempObject[category][currentField.name]) : 
+                        $filter('date')(tempObject[category][currentField.name], DATE_FORMAT);
+                    }
+                }
+                
+            });
+
+            return tempObject;
+        }
+
+        //returns the field object from the array of specified category
+        $scope.getField = function (category, fieldName) {
+            return $scope.fields[category].find(function(field) {
+                return field.name === fieldName;
+            })
+        }
 
     }
 })();
