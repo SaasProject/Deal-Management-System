a7a36d008ceafcb0034b9a711a2876efed4756ea
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2018-06-07 14:43:20 +0800

    finished computation in distribution

--- app/deals/dealForm.js
+++ app/deals/dealForm.js
@@ -33,20 +33,24 @@
             direct: 'Direct to Client',
             middle1: 'To Middleman',
             middle2: 'Middleman to Client'
-        }
+        };
+
         $scope.contracts = [distributionStrings.direct];
 
-        $scope.validateBlankInputs = function(){
+        //store total computations outside of dealForm.distribution to avoid registering changes in $scope.$watch
+        //set this to dealForm.distribution[dealForm.process['SOW Scheme']].total during submit
+        $scope.total = {};
+
+        $scope.validateBlankInputs = function () {
             console.log('glenn');
             //glenn's code to loop category fields if there is required
             var targetCategory = ['profileSection', 'processSection', 'distributionSection', 'statusSection', 'contentSection'];
 
-            for (var a = 0; a < targetCategory.length; a++){
+            for (var a = 0; a < targetCategory.length; a++) {
                 var categoryInputs = document.getElementById(targetCategory[a]).querySelectorAll('input,textarea,select');
-                var inputLength = categoryInputs.length;
-                for (var i = 0; i < inputLength; i++) {
-                    if (categoryInputs[i].value === '' && categoryInputs[i].required){
-                        $('#'+targetCategory[a]).collapse('show');
+                for (var i = 0; i < categoryInputs.length; i++) {
+                    if (categoryInputs[i].value === '' && categoryInputs[i].required) {
+                        $('#' + targetCategory[a]).collapse('show');
                         break;
                     }
                 }
@@ -54,32 +58,37 @@
         }
 
         //get the fields arrays of dealessential, dealprofile, dealprocess, dealstatus, and dealcontent
-        function getAllFields () {
+        function getAllFields() {
             ModulesService.getAllModules().then(function (allModules) {
                 var category;
-                var allModulesLength = allModules.length;
-                for (var i = 0; i < allModulesLength; i++) {
+                for (var i = 0; i < allModules.length; i++) {
                     //first condition is to make sure that collections that have 'deal' in its name are only processed.
                     if (allModules[i].name.search('deal') !== -1 && allModules[i].name !== 'deals') {
                         category = allModules[i].name.replace('deal', '');
                         $scope.fields[category] = allModules[i].fields;
                     }
                 }
+
+                //initialize $scope.total based from SOW Scheme options here
+                var sowSchemeStrings = $scope.getField('process', 'SOW Scheme').options;
+                for (var j = 0; j < sowSchemeStrings.length; j++) {
+                    $scope.total[sowSchemeStrings[j]] = {};
+                }
             }).catch(function (err) {
             });
         }
 
         getAllFields();
-        
+
         //if there is a parameter, it means that a deal is going to be updated
         if ($stateParams.ID !== '') {
             //get one then store to $scope.dealForm;
-            DealsService.getDealById($stateParams.ID).then(function(aDeal) {
+            DealsService.getDealById($stateParams.ID).then(function (aDeal) {
                 //use true to convert datestrings to date objects
                 $scope.dealForm = preProcess(aDeal, true);
 
                 console.log($scope.dealForm);
-             }).catch(function() {
+            }).catch(function () {
                 $scope.message = 'Cannot find the deal';
             });
         }
@@ -87,28 +96,28 @@
         $scope.submit = function () {
             //use Object.assign(target, source) instead
             var tempDealForm = {};
-            Object.assign(tempDealForm, $scope.dealForm);            
+            Object.assign(tempDealForm, $scope.dealForm);
 
             //use false to convert date objects to datestrings
             try {
                 tempDealForm = preProcess(tempDealForm, false);
                 if (tempDealForm._id === undefined) {
                     DealsService.addDeal(tempDealForm)
-                    .then(function() {
-                        $state.transitionTo('dealList');
-                    })
-                    .catch(function(err) {
-        
-                    });
+                        .then(function () {
+                            $state.transitionTo('dealList');
+                        })
+                        .catch(function (err) {
+
+                        });
                 } else {
                     DealsService.updateDeal(tempDealForm)
-                    .then(function() {
-                        $state.transitionTo('dealList');
-                    })
-                    .catch(function() {
-        
-                    });
-                }     
+                        .then(function () {
+                            $state.transitionTo('dealList');
+                        })
+                        .catch(function () {
+
+                        });
+                }
             } catch (e) {
                 //console.log(e);
                 $scope.message = e.message;
@@ -128,12 +137,7 @@
 
                 },
                 distribution: {
-                    /* total: {
-                        resource: 0,
-                        revenue: 0,
-                        cm: 0,
-                        percentage: 0
-                    },
+                    /*
                     average: 0,
                     cm: {},
                     res: {
@@ -157,23 +161,23 @@
         $scope.getCurrentFiscalYear = function () {
             //reset the array
             $scope.currentFiscalYear = [];
-            
+
             //assign to the current date if input is null
             if ($scope.startingMonthYear === null) {
                 $scope.startingMonthYear = new Date();
             }
             //+ 1 since javascript months starts with 0
-            var setMonth = $scope.startingMonthYear.getMonth() + 1; 
+            var setMonth = $scope.startingMonthYear.getMonth() + 1;
             var setYear = $scope.startingMonthYear.getFullYear();
             var temp = '';
             var i;
-            
+
             i = setMonth;
             do {
                 //next year
                 if (i < setMonth) {
                     temp = (i < 10) ? ('0' + i + '/' + (setYear + 1)) : (i + '/' + (setYear + 1));
-                //current year
+                    //current year
                 } else {
                     temp = (i < 10) ? ('0' + i + '/' + setYear) : (i + '/' + setYear);
                 }
@@ -188,7 +192,6 @@
         $scope.getCurrentFiscalYear();
 
         function preProcess(dealForm, isLoaded) {
-            console.log('jeremy');
             var tempObject = dealForm;
 
             //perform operations on variables that you can explicitly determine
@@ -196,19 +199,22 @@
             if (isLoaded) {
                 //set the sow scheme (for the distribution table)
                 $scope.setContracts(tempObject.process['SOW Scheme']);
-            //during submit
+                //during submit
             } else {
                 //explicitly set SOW scheme (because default selected options is not working) to Direct
                 if (tempObject.process['SOW Scheme'] === null || tempObject.process['SOW Scheme'] === undefined) {
                     tempObject.process['SOW Scheme'] = 'Direct';
                 }
-                
+
                 //throw error if start date > end date
-                if(tempObject.profile['Duration (Start)'] > tempObject.profile['Duration (End)']) {
+                if (tempObject.profile['Duration (Start)'] > tempObject.profile['Duration (End)']) {
                     throw new Error('End date must be greater than or equal to the start date');
                 }
 
+                //set $scope.total to dealForm.distribution.total
+
                 //delete the other to avoid being included in computation and being stored in database
+                //kung hindi pwde sabay ang direct & indirect, uncomment this
                 /* if (tempObject.process['SOW Scheme'] === 'Indirect') {
                     delete tempObject.distribution[distributionStrings.direct];
                 } else {
@@ -218,39 +224,38 @@
             }
 
             //use $scope.fields and iterate each array
-            angular.forEach($scope.fields, function(fields, category) {
-                console.log(category);
-                var i, currentField, arrayLength = fields.length;
-                for(i = 0; i < arrayLength; i++) {
+            angular.forEach($scope.fields, function (fields, category) {
+                var i, currentField;
+                for (i = 0; i < fields.length; i++) {
                     currentField = $scope.fields[category][i];
 
                     //not working, workaround is to use validateBlankInputs() during ng-click
-                   /*  if(!isLoaded && currentField.required && 
-                        (tempObject[category][currentField.name] === '' ||
-                        tempObject[category][currentField.name] === null || 
-                        tempObject[category][currentField.name] === undefined)) {
-                            throw {REQUIRED_ERROR: true, category: category};
-                    } */
+                    /*  if(!isLoaded && currentField.required && 
+                         (tempObject[category][currentField.name] === '' ||
+                         tempObject[category][currentField.name] === null || 
+                         tempObject[category][currentField.name] === undefined)) {
+                             throw {REQUIRED_ERROR: true, category: category};
+                     } */
 
-                    
+
                     //preprocess when loading
                     if (isLoaded) {
-                        if(currentField.type === 'date' && tempObject[category][currentField.name] !== null) {
+                        if (currentField.type === 'date' && tempObject[category][currentField.name] !== null) {
                             tempObject[category][currentField.name] = new Date(tempObject[category][currentField.name]);
                         }
-                    //preprocess during submit
+                        //preprocess during submit
                     } else {
                         //do not store keys with null values
                         if (tempObject[category][currentField.name] === null) {
                             delete tempObject[category][currentField.name];
                         }
 
-                        if(currentField.type === 'date' && tempObject[category][currentField.name] !== null) {
+                        if (currentField.type === 'date' && tempObject[category][currentField.name] !== null) {
                             tempObject[category][currentField.name] = $filter('date')(tempObject[category][currentField.name], DATE_FORMAT);
-                        } 
+                        }
                     }
                 }
-                
+
             });
 
             return tempObject;
@@ -258,22 +263,130 @@
 
         //returns the field object from the array of specified category
         $scope.getField = function (category, fieldName) {
-            return $scope.fields[category].find(function(field) {
+            return $scope.fields[category].find(function (field) {
                 return field.name === fieldName;
             });
         }
 
         $scope.setContracts = function (sowScheme) {
             //$scope.contracts = [];
-            
+
             //why is it not working huhu
             //always else huhu
-            if(sowScheme === 'Indirect') {
+            if (sowScheme === 'Indirect') {
                 $scope.contracts = [distributionStrings.middle1, distributionStrings.middle2];
-            } else {                
+            } else {
                 $scope.contracts = [distributionStrings.direct];
             }
         }
 
+        // place computeDistribution in $scope.setContracts because it doesnt work here :(
+        /* $scope.$watch("dealForm.process.['SOW Scheme']", function() {
+            computeDistribution();
+        }); */
+        //use true to register changes in values
+        $scope.$watch('dealForm.distribution', function () {
+            //if distribution is empty, do not compute
+            if (!angular.equals($scope.dealForm.distribution, {})) {
+                computeDistribution();
+            }
+        }, true);
+
+        function computeDistribution() {
+            //console.log($scope.dealForm.distribution);
+            //use variables like sumRes as temporary sum
+            var i, resJP = [], resGD = [], revJP = [], revGD = [], cm = [];
+            var resSum = 0, revSum = 0, cmSum = 0;
+            //for direct or indirect
+            for (i = 0; i < $scope.contracts.length; i++) {
+                //console.log($scope.dealForm.distribution[$scope.contracts[i]].res);
+                //compute total resource
+                if ($scope.dealForm.distribution[$scope.contracts[i]] !== undefined) {
+                    /**
+                     * check if not undefined to avoid displaying errors
+                     * for res & rev, need to check both jp & gd if not undefined to avoid errors
+                     * */
+                    //use object.values since they are all integers
+                    //check resources of JP
+                    if ($scope.dealForm.distribution[$scope.contracts[i]].res !== undefined) {
+                        if ($scope.dealForm.distribution[$scope.contracts[i]].res.jp !== undefined) {
+                            resJP = Object.values($scope.dealForm.distribution[$scope.contracts[i]].res.jp);
+                        }
+
+                        //check resources of GD
+                        if ($scope.dealForm.distribution[$scope.contracts[i]].res.gd !== undefined) {
+                            resGD = Object.values($scope.dealForm.distribution[$scope.contracts[i]].res.gd);
+                        }
+                    }
+
+                    if ($scope.dealForm.distribution[$scope.contracts[i]].rev !== undefined) {
+                        //check revenue of JP
+                        if ($scope.dealForm.distribution[$scope.contracts[i]].rev.jp !== undefined) {
+                            revJP = Object.values($scope.dealForm.distribution[$scope.contracts[i]].rev.jp);
+                        }
+                        //check revenue of GD
+                        if ($scope.dealForm.distribution[$scope.contracts[i]].rev.gd !== undefined) {
+                            revGD = Object.values($scope.dealForm.distribution[$scope.contracts[i]].rev.gd);
+                        }
+                    }
+
+                    //check cm
+                    if ($scope.dealForm.distribution[$scope.contracts[i]].cm !== undefined) {
+                        cm = Object.values($scope.dealForm.distribution[$scope.contracts[i]].cm);
+                    }
+                }
+            }
+
+            //compute total resource
+            if (resJP.length > 0 || resGD.length > 0) {
+                resSum = resJP.concat(resGD).reduce(function (total, value) {
+                    return (value !== undefined && value !== null) ? total + value : total + 0;
+                });
+            }
+
+            //compute total revenue
+            if (revJP.length > 0 || revGD.length > 0) {
+                revSum = revJP.concat(revGD).reduce(function (total, value) {
+                    return (value !== undefined && value !== null) ? total + value : total + 0;
+                });
+            }
+
+            //compute average
+            var average = revSum / resSum;
+            //check average value
+            /* if(isNaN(average) || average === Infinity) {
+                average = 0;
+            } */
+
+            //compute total cm
+            if (cm.length > 0) {
+                cmSum = cm.reduce(function (total, value) {
+                    return (value !== undefined && value !== null) ? total + value : total + 0;
+                });
+            }
+
+            console.log(resSum, revSum, cmSum);
+
+
+            /**
+             * nakakapagcompute na ng total resource. considered na lahat ng months ng both jp & gd.
+             * kaso kapag nagswitch yung SOW Scheme, magrerecompute sya at maooverwrite yung lumang totals
+             * e.g. put some values when SOW Scheme is undefined/Direct. kapag magsswitch,
+             * hindi nakasave sa variable yung total ni direct. magrerecompute ulit sya kapag naging direct ulit
+             * yung sow scheme, which is inefficient
+             */
+
+            //if sir bert states that these should be stored in the database, do this.
+            //if not, diretso scope.total tapos hindi iseset sa dealForm.distribution
+            if ($scope.dealForm.process['SOW Scheme'] !== undefined) {
+                $scope.total[$scope.dealForm.process['SOW Scheme']].resource = (resSum !== null) ? resSum : 0;
+                $scope.total[$scope.dealForm.process['SOW Scheme']].revenue = (revSum !== null) ? revSum : 0;
+                //((revSum / resSum) !== NaN) does not work
+
+                $scope.total[$scope.dealForm.process['SOW Scheme']].average = average;
+                $scope.total[$scope.dealForm.process['SOW Scheme']].cm = (cmSum !== null) ? cmSum : 0;
+                console.log($scope.total);
+            }
+        }
     }
 })();
