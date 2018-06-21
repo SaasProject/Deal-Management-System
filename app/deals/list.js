29a1908d53feaf535f3b2acafef43dd7aacddead
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2018-06-21 14:34:02 +0800

    implemented display options

--- app/deals/list.js
+++ app/deals/list.js
@@ -48,19 +48,69 @@
 
         $scope.reverse = false;
 
-        function getAllDeals () {
+        $scope.displayOption = 'Active';
+
+        $scope.getDeals = function () {
             ModulesService.getAllModuleDocs('deals').then(function (allDeals) {
                 $scope.deals = allDeals;
-                //compare to '9' not 9 because dropdown values are strings :(
-                $scope.deals = $scope.deals.filter(function(aDeal) {
-                    return aDeal.profile['Level'] !== '9';
-                });
+                switch($scope.displayOption) {
+                    //display levels 2,3,4,5 only
+                    case 'Active': {
+                        $scope.deals = $scope.deals.filter(function(aDeal) {
+                            return aDeal.profile['Level'] !== '1' && aDeal.profile['Level'] !== '9';
+                        });
+                    }break;
+                    //1 - same as active
+                    //2 - showing 1 month (up to next month) (active deals that are due before next month)
+                    //3 - unclosed deals whose due date is before this month
+                    case '1 Month': {
+                        var nextMonth = moment().add(1, 'months').endOf('month');
+                        //console.log(nextMonth.month());
+                        var diff;
+                        $scope.deals = $scope.deals.filter(function(aDeal) {
+                            //using 'months' in .diff() results to 0 for the next month and next next month
+                            //e.g. current month is june, so july 31 is next month
+                            //if this is used, deals due on august are also included
+                            diff = nextMonth.diff(aDeal.essential['Due Date'], 'months', true);
+                            //console.log(diff);
+                            return ((aDeal.profile['Level'] !== '1' && aDeal.profile['Level'] !== '9') && 
+                            (diff >= 0));
+                        });
+                    }break;
+                    case '2-3 Months': {
+                        var nextMonth = moment().add(3, 'months').endOf('month');
+                        //console.log(nextMonth.month());
+                        var diff;
+                        $scope.deals = $scope.deals.filter(function(aDeal) {
+                            //use true to get decimal places. this is to avoid having 0s for adjacent months
+                            //i.e. next month & next next month
+                            diff = nextMonth.diff(aDeal.essential['Due Date'], 'months', true);
+                            //console.log(diff);
+                            return ((aDeal.profile['Level'] !== '1' && aDeal.profile['Level'] !== '9') && 
+                            (diff >= 0 && diff <= 2));
+                        });
+                    }break;
+                    case 'Over Due': {
+                        $scope.deals = $scope.deals.filter(function(aDeal) {
+                            return aDeal.profile['Level'] !== '1' && 
+                            aDeal.profile['Level'] !== '9' && 
+                            moment().diff(aDeal.essential['Due Date'], 'days') > 0;
+                        });
+                    }break;
+                    
+                    case 'Closed in 2018': {
+                        $scope.deals = $scope.deals.filter(function(aDeal) {
+                            return aDeal.profile['Level'] === '1' && 
+                            moment(aDeal.closedDate).year() === new Date().getFullYear();
+                        });
+                    }
+                }
             }).catch(function (err) {
 
             });
         }
 
-        getAllDeals();
+        $scope.getDeals();
 
         function getAllFields () {
             ModulesService.getAllModules().then(function (allModules) {
